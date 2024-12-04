ab0d314f896af22e985aa9f52852cfb0659904cf
Osotatanekeeni <tatanekari52@gmail.com>
2024-12-04 22:49:04 +0200

    Added initial hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "./components/HeroSection";
+
 function App() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
+      <HeroSection />
     </main>
   );
 }
